f1e48bef53e99f462737cabe92f06cf35d7130f4
Irakli Dolbaia <dolbaia73@gmail.com>
2025-09-18 11:44:24 +0400

    solution for day02 practice_02

---
+++ day_02/Practice_02/Practice_02/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Hello, TBC!");
+
+Console.WriteLine("Input the year: ");
+int year = Convert.ToInt32(Console.ReadLine());
+
+// using mod operator to check if the year is a leap year
+
+bool iss = (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0); 
+
+Console.WriteLine(!iss ? "False" : "True");
