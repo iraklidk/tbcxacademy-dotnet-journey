aa64d7e6f8f696f1324622dd70f29f9700950e47
Irakli Dolbaia <dolbaia73@gmail.com>
2025-09-21 23:11:02 +0400

    solution for pr 02

--- 03 - If-Else, Switch, Type Casting/Practice_01/Practice_01/Practice_02.cs
+++ 03 - If-Else, Switch, Type Casting/Practice_01/Practice_01/Practice_02.cs
@@ -1,7 +1,14 @@
 ﻿public class Practice_02
 {
-    public static void Main2(string[] args)
+    public static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Enter first number :");
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter second number :");
+        int b = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter third number :");
+        int c = Convert.ToInt32(Console.ReadLine());
+        if(a + b > c && a + c > b && b + c > a) Console.WriteLine("This should be a triangle !");
+        else Console.WriteLine("This shouldn't be a triangle !");
     }
 }
