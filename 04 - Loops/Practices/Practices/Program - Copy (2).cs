f4c614790578193eb61b80a75bccc3011ea0c77c
Irakli Dolbaia <dolbaia73@gmail.com>
2025-09-23 12:13:03 +0400

    practices

---
+++ 04 - Loops/Practices/Practices/Program - Copy (2).cs
@@ -0,0 +1,16 @@
+﻿public class Practice_05
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a number of rows of Floyd's triangle to be printed: ");
+        int k = Convert.ToInt32(Console.ReadLine());
+        for(int i = 1; i <= k; ++i)
+        {
+            for(int j = 1; j <= i; ++j)
+            {
+
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ 04 - Loops/Practices/Practices/Program - Copy (4).cs
@@ -0,0 +1,18 @@
+﻿public class Practice_01_021234
+{
+    public static int sum(int n)
+    {
+        return n * (n + 1) / 2;
+    }
+
+    public static void Main4(string[] args)
+    {
+        Console.WriteLine(10); // 01
+
+        int k = Convert.ToInt32(Console.ReadLine());
+
+
+        Console.WriteLine("Sum from 1 to " + k + " is: ");
+        Console.WriteLine(sum(k)); // 02
+    }
+}
