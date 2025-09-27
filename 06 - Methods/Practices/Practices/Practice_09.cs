5e115c41901c428f139bc46598a3082ce83d7213
Irakli Dolbaia <dolbaia73@gmail.com>
2025-09-28 02:07:37 +0400

    sol for pr 09

---
+++ 06 - Methods/Practices/Practices/Practice_09.cs
@@ -0,0 +1,18 @@
+﻿public class Practice_09
+{
+    static Int32 prod(params int[] arr)
+    {
+        Int32 res = 1;
+        foreach(int n in arr)
+        {
+            res *= n;
+        }
+        return res;
+    }
+
+    public static void Main7(string[] args)
+    {
+        int [] arr = new int[] { 1, 3, 4, 15, 13, 23, 98 };
+        Console.WriteLine("The product of array elements is: " + prod(arr));
+    }
+}
