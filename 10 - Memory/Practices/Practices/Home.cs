fd3baf8af788d670458838613923d3fa605cdcd0
Irakli Dolbaia <dolbaia73@gmail.com>
2025-10-07 17:44:14 +0400

    sol for 10 - memory

---
+++ 10 - Memory/Practices/Practices/Home.cs
@@ -0,0 +1,14 @@
+﻿namespace Practices
+{
+    public class Home
+    {
+        public string Adress { get; set; }
+        public string City { get; set; }
+
+        public Home(string adress, string city)
+        {
+            Adress = adress;
+            City = city;
+        }
+    }
+}
---
+++ 10 - Memory/Practices/Practices/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practices
+{
+    public class Person
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+        public int Personal_ID_Number { get; set; }
+        public Home Home { get; set; }
+        public Person(int age, string name, int personalIdNumber)
+        {
+            Age = age;
+            Name = name;
+            Personal_ID_Number = personalIdNumber;
+        }
+    }
+}
