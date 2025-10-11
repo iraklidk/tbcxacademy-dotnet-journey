a105df6e345b4ae41f99b0b9f40e2a8272b5f4ba
Irakli Dolbaia <dolbaia73@gmail.com>
2025-10-11 12:10:27 +0400

    solve for 01

---
+++ Midterm 1/Midterm 1/Midterm 1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Numerics;
+
+public class Midterm_01
+{
+    public static void solve(int[] arr, int target)
+    {
+        int left = 0, right = arr.Length - 1;
+        while(left < right)
+        {
+            if (arr[left] + arr[right] == target) Console.WriteLine(arr[left] + ", " + arr[right]);
+            if(arr[left] + arr[right] > target) right--;
+            else left++;
+        }
+    }
+    public static void test()
+    {
+        Console.WriteLine("test");
+    }
+
+    public static void Main()
+    {
+        int[] some = { 1, 2, 3, 4, 5, 6, 7, 8 };
+        Console.Write("input the targeT: ");
+        int target = Convert.ToInt32(Console.ReadLine());
+
+        solve(some, target);
+    }
+}
