e90a23fac0999dee0ec3871dd2f41a6b0ae87337
Irakli Dolbaia <dolbaia73@gmail.com>
2025-10-19 22:16:19 +0400

    solve for 13 - 02

---
+++ 13 - OOP 2 & 3/Practice 2/Practice/CollectionExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    public static class CollectionExtensions
+    {
+        public static int[] MergeSequence(this int[] array, int[] anotherArray)
+        {
+            int sizeArray = array.Length;
+            int sizeAnotherArray = anotherArray.Length;
+            int[] newArray = new int[sizeArray + sizeAnotherArray];
+            for (int i = 0; i < sizeArray; ++i)
+            {
+                newArray[i] = array[i];
+            }
+            for (int j = 0; j < sizeAnotherArray; ++j)
+            {
+                newArray[sizeArray + j] = anotherArray[j];
+            }
+
+            return newArray;
+        }
+    }
+}
---
+++ 13 - OOP 2 & 3/Practice 2/Practice/StringExtensions.cs
@@ -0,0 +1,26 @@
+﻿namespace Practice
+{
+    public static class StringExtensions
+    {
+        public static string ReverseString(this string str)
+        {
+            char[] charArray = str.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+
+        public static int CountCharacter(this string str, char ch)
+        {
+            int count = 0;
+            foreach(char c in str) if (c == ch) ++count;
+            return count;
+        }
+
+        public static bool StartOrEndGivenSubstring(this string str, string substr)
+        {
+            int n = substr.Length;
+            if(str.Length < n) return false;
+            return str.Substring(0, n) == substr || str.Substring(str.Length - n) == substr;
+        }
+    }
+}
