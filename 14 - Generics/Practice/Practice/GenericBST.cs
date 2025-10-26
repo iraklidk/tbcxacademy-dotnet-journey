e2065fdfc6f62bbeb4f3a320b15bf8ff535b3d08
Irakli Dolbaia <dolbaia73@gmail.com>
2025-10-26 16:15:23 +0400

    AVL optimization for bst

--- 14 - Generics/Practice/Practice/GenericBST.cs
+++ 14 - Generics/Practice/Practice/GenericBST.cs
@@ -5,9 +5,11 @@
         public T Value;
         public Node<T> Left;
         public Node<T> Right;
+        public int Height; // for AVL tracking
         public Node(T value, Node<T> left = null, Node<T> right = null)
         {
             Value = value;
+            Height = 1;
         }
     }
 
@@ -24,14 +26,8 @@
         {
             if(root is null) return null;
             if(root.Value.CompareTo(value) == 0) return root;
-            else if(value.CompareTo(root.Value) < 0)
-            {
-                return SearchDFS(root.Left, value);
-            }
-            else
-            {
-                return SearchDFS(root.Right, value);
-            }
+            else if(value.CompareTo(root.Value) < 0) return SearchDFS(root.Left, value);
+            else return SearchDFS(root.Right, value);
         }
 
         public void Insert(T value)
@@ -40,28 +36,14 @@
         }
         private Node<T> InsertDFS(Node<T> root, T value)
         {
-            if(root is null)
-            {
-                return new Node<T>(value);
-            }
-            else if(value.CompareTo(root.Value) < 0)
-            {
-                root.Left = InsertDFS(root.Left, value);
-            }
-            else
-            {
-                root.Right = InsertDFS(root.Right, value);
-            }
+            if(root is null) return new Node<T>(value);
+            else if(value.CompareTo(root.Value) < 0) root.Left = InsertDFS(root.Left, value);
+            else root.Right = InsertDFS(root.Right, value);
 
-            return root;
+            return root.BalanceNode(); // ipdate height and rebalance avl subtree
         }
 
-
-
-        public void Delete(T value)
-        {
-            Root = DeleteDFS(Root, value);
-        }
+        public void Delete(T value) => Root = DeleteDFS(Root, value);
         private Node<T> DeleteDFS(Node<T> root, T value)
         {
             if(root is null) return root;
@@ -73,12 +55,12 @@
             {
                 if(root.Left is null) return root.Right;
                 if(root.Right is null) return root.Left;
-                Node<T> minNode = GetMinNode(root.Right); // get right subtrees minimum node
+                Node<T> minNode = GetMinNode(root.Right); // get right subtree's minimum node
                 root.Value = minNode.Value;
                 root.Right = DeleteDFS(root.Right, minNode.Value);
             }
 
-            return root;
+            return root.BalanceNode(); // rebalance
         }
         private Node<T> GetMinNode(Node<T> node)
         {
