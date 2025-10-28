c58cc2cc92f0102304752d2a35706b5a628f43fe
iraklidk <dolbaia73@gmail.com>
2025-10-29 01:48:01 +0400

    solve for 15

---
+++ 15 - Collections and Data Structures/Practice/Practice/BookComparer.cs
@@ -0,0 +1,20 @@
+﻿using static CustomSort;
+
+internal class DateComparer : IComparer<Book>
+{
+    public int Compare(Book x, Book y)
+    {
+        if (x == null || y == null) return 0;
+
+        return x.ReleaseDate.CompareTo(y.ReleaseDate);
+    }
+}
+
+internal class ISBNComparer : IComparer<Book>
+{
+    public int Compare(Book x, Book y)
+    {
+        if (x == null || y == null) return 0;
+        return string.Compare(x.ISBN, y.ISBN, StringComparison.Ordinal);
+    }
+}
---
+++ 15 - Collections and Data Structures/Practice/Practice/PointCalculations.cs
@@ -0,0 +1,18 @@
+﻿internal class PointCalculations
+{
+    static double GetDistance((double x, double y) a, (double x, double y) b)
+           => Math.Sqrt((a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y));
+
+    static void Main2()
+    {
+        var tests = new ((double, double), (double, double))[]
+        {
+            ((0, 0), (3, 4)),
+            ((-1, -1), (2, 3)),
+            ((2.5, 4.5), (5.5, 8.5)),
+            ((1000, 1000), (1003, 1004))
+        };
+
+        foreach (var (a, b) in tests) Console.WriteLine($"From {a} to {b} -> {GetDistance(a, b)}\n");
+    }
+}
