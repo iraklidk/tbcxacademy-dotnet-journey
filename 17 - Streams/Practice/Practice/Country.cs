d7288ef42162103e45a4dcbe8ac044315cbb1486
iraklidk <dolbaia73@gmail.com>
2025-11-05 18:00:15 +0400

    solve for 17 - streams

---
+++ 17 - Streams/Practice/Practice/Country.cs
@@ -0,0 +1,16 @@
+﻿namespace Practice
+{
+    internal class Country : GeographicEntity
+    {
+        public List<City> Cities { get; set; }
+        public Country(string name, List<City> cities) : base(name)
+        {
+            foreach(City city in cities)
+            {
+                Area += city.Area;
+                Population += city.Population;
+            }
+            Cities = cities;
+        }
+    }
+}
---
+++ 17 - Streams/Practice/Practice/CountryMustHaveSingleCapitalException.cs
@@ -0,0 +1,8 @@
+﻿namespace Practice
+{
+    internal class CountryMustHaveSingleCapitalException : Exception
+    {
+        public CountryMustHaveSingleCapitalException()
+            : base("A country must have exactly one capital city.") { }
+    }
+}
